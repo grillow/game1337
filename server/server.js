98ffbd8ef4d32577b549a2f4fc4058d9e7bc42c8
grillow <spmr757@gmail.com>
2021-07-03 00:37:43 +0300

    for docker

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use('/game', express.static(__root + '/game'))
 
 const httpServer = require('http').createServer(app);
 const PORT = process.env.PORT || 8080;
-const HOST = process.env.HOST || localhost;
+const HOST = process.env.HOST || '0.0.0.0';
 httpServer.listen(PORT, HOST);
 console.log(`Server started at ${HOST}:${PORT}`);
 
